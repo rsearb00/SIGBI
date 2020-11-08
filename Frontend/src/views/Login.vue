21ef475ff6c72d6555e8636a9433aa34e77b35a9
rsearb00 <rsearb00@estudiantes.unileon.es>
2020-11-08 17:30:20 +0100

    Login conseguido, interfaz beta

---
+++ Frontend/src/views/Login.vue
@@ -0,0 +1,117 @@
+<template>
+  <v-app id="inspire">
+    <v-content>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="8" md="4">
+            <v-alert :type="tipoAlerta" v-if="alerta" dismissible>{{
+              textoAlerta
+            }}</v-alert>
+            <v-card class="elevation-12">
+              <v-toolbar color="#4D5E9C" dark flat>
+                <v-toolbar-title>Iniciar Sesión</v-toolbar-title>
+                <v-spacer />
+                
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field
+                    label="Usuario"
+                    name="login"
+                    prepend-icon="mdi-account"
+                    type="text"
+                    v-model="id"
+                  />
+
+                  <v-text-field
+                    id="password"
+                    label="Contraseña"
+                    name="password"
+                    prepend-icon="mdi-lock"
+                    type="password"
+                    v-model="contrasenya"
+                    v-on:keyup.enter="clickeo"
+                  />
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer />
+                <v-btn dark color="#4D5E9C" @click="clickeo()">Entrar</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+// @ is an alias to /src
+const axios = require("axios");
+export default {
+  name: "Login",
+  components: {},
+  data: () => ({
+    id: "",
+    contrasenya: "",
+    alerta: false,
+    tipoAlerta: "",
+    textoAlerta: ""
+  }),
+  methods: {
+    clickeo: function() {
+      this.alerta = false;
+      if (this.id == "" && this.contrasenya == "") {
+        this.tipoAlerta = "error";
+        this.alerta = true;
+        this.textoAlerta = "Por favor, introduce usuario y contraseña";
+      } else if (this.id == "") {
+        this.tipoAlerta = "error";
+        this.alerta = true;
+
+        this.textoAlerta = "Por favor, introduce tu usuario";
+      } else if (this.contrasenya == "") {
+        this.tipoAlerta = "error";
+        this.alerta = true;
+        this.textoAlerta = "Por favor, introduce tu contraseña";
+      } else {
+        console.log("Intento de inicio de sesión");
+        axios
+          .post("http://localhost:3000/login", {
+            user: this.id,
+            password: this.contrasenya
+          })
+          .then(response => {
+            //Llamada exitosa
+            if (response.data.ok) {
+              this.tipoAlerta = "success";
+              console.log(response.data.ok + " Inicio de sesión correcto");
+              this.alerta = true;
+              this.textoAlerta =
+                "¡Bienvenido " + this.id + "!";
+              /*setTimeout(() => {
+                this.$router.push({
+                  name: "Inicio",
+                  params: { idUsuario: this.id }
+                });
+              }, 750);*/
+            } else {
+              this.tipoAlerta = "error";
+              this.alerta = true;
+              this.textoAlerta = "Los datos introducidos son incorrectos";
+              console.log(response.data.ok + " Fallo en el inicio de sesión");
+            }
+          })
+          .catch(alerta => {
+            //alerta
+            console.log(alerta);
+          });
+      }
+    }
+  },
+  mounted: function() {}
+};
+</script>
+
+<style lang="scss" scoped></style>
